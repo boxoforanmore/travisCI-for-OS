854bf87c1a37b7a867326a1203949727742bf4c8
Peter Nielson <computergeekpete@gmail.com>
2018-06-14 19:17:10 -0600

    Changed files to use .cpp vs .cc

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 echo
 echo
-echo "Running: gcc -o main main.cc -Wall"  `gcc -o main main.cc -Wall`
-echo "Running: gcc -o child child.cc -Wall"  `gcc -o child child.cc -Wall`
+echo "Running: gcc -o main main.cpp -Wall"  `gcc -o main main.cpp -Wall`
+echo "Running: gcc -o child child.cpp -Wall"  `gcc -o child child.cpp -Wall`
 echo
 echo
 ./main
