cc486307dba19457eaf503ac53b6a3a8c6b9ddbb
Peter Nielson <computergeekpete@gmail.com>
2018-06-14 20:06:51 -0600

    Fixed order of compilation

--- travis.sh
+++ travis.sh
@@ -2,11 +2,12 @@
 echo "Installing clang-tidy" `sudo apt-get install clang-tidy`
 echo
 echo
+echo "Running: gcc -o child child.c -Wall"  `gcc -o child child.c -Wall`
+echo "Running: gcc -o main main.c -Wall"  `gcc -o main main.c -Wall`
+echo
 echo "Running clang-tidy main.c" `clang-tidy main.c`
 echo "Running clang-tidy child.c" `clang-tidy child.c`
 echo
-echo "Running: gcc -o child child.c -Wall"  `gcc -o child child.c -Wall`
-echo "Running: gcc -o main main.c -Wall"  `gcc -o main main.c -Wall`
 echo
 echo
 ./main
