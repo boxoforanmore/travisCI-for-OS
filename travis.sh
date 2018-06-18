6b07f9403b6d9ac4e630858fb0384f35bd96141e
Peter Nielson <computergeekpete@gmail.com>
2018-06-18 11:36:21 -0600

    Added clang and valgrind install and code to script and travis yaml

--- travis.sh
+++ travis.sh
@@ -5,9 +5,11 @@ echo
 echo "Running: gcc -o child child.c -Wall"  `gcc -o child child.c -Wall`
 echo "Running: gcc -o main main.c -Wall"  `gcc -o main main.c -Wall`
 echo
-echo "Running clang-tidy main.c" `clang-tidy main.c`
-echo "Running clang-tidy child.c" `clang-tidy child.c`
+echo "Running clang-tidy on main.c" `clang-tidy main.c -- -Imy_project/include -DMY_DEFINES ...`
+echo "Running clang-tidy on child.c" `clang-tidy child.c -- -Imy_project/include -DMY_DEFINES ...`
 echo
+echo 
+echo "Running valgrind on ./main" `valgrind --leak-check=yes ./main`
 echo
 echo
 ./main
